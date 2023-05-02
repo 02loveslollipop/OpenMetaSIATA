f1756bfc382c136c176cd4ef38c585d3242ac7f8
02loveslollipop <mmonsalve2005@gmail.com>
2023-05-02 01:28:35 -0500

    Delete if that isn't needed

--- api/api.py
+++ api/api.py
@@ -1,38 +1,14 @@
-'''
-import flask as f
-import pandas as pd
-url = 'http://siata.gov.co:8089/estacionesNivel/cf7bb09b4d7d859a2840e22c3f3a9a8039917cc3/?format=json'
-
-webCapture = pd.read_json(url,convert_dates=True)
-api = f.Flask(__name__)
-HighLevel = []
-
-@api.route('/getStationsLevels')
-def showStations():
-    for station in webCapture:
-        if station['porcentajeNivel'] >= 2:
-            pass
-            #TODO: Realizar la implementacion de contar los porcentajes
-    token = f.request.args
-    
-    return webCapture.to_string()
-
-if __name__ == '__main__':
-    api.run(host='0.0.0.0',port=6969)
-'''
 from flask import Flask, request, jsonify
 from confLoader import ConfLoader
 from updateThread import UpdateQueue
 
 app = Flask(__name__)
 
-setup = False
-if not setup:
-    config = ConfLoader() #The class load the config of config.yml
-    stationList = []
-    thread = UpdateQueue(config=config,stationList=stationList)
-    thread.run()
-    setup = True
+config = ConfLoader() #The class load the config of config.yml
+stationList = []
+thread = UpdateQueue(config=config,stationList=stationList)
+thread.run()
+
 
 # The route in this case CheckLevel, and the index as part of the route, the token is pass in the header of the GET request
 @app.route('/CheckLevel/<int:index>', methods=['GET'])
