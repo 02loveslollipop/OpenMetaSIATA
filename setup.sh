5bddb238f8d7f9b750864aee18a3adfbf78d429d
02loveslollipop <mmonsalve2005@gmail.com>
2023-05-07 00:32:45 -0500

    Updated Dockerfile

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt update
 sudo apt install docker-compose
-sudo docker build -t OpenMetaSIATA/API:1.0.0beta api/ -p 5000:5000
-sudo docker build -t OpenMetaSIATA/DB:1.0.0beta db/ -p 6969:6969
-sudo docker build -t OpenMetaSIATA/WEB:1.0.0beta webPage/ -p 80:80
+sudo docker build -t openmetasiata/api:1.0.0beta api/ 
+sudo docker build -t openmetasiata/db:1.0.0beta db/ 
+sudo docker build -t openmetasiata/api:1.0.0beta webPage/ 
