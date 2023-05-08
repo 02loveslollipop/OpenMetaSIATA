527a1f6bb1d91b02a723597d5c9cbf495ef39b64
02loveslollipop <mmonsalve2005@gmail.com>
2023-05-08 00:38:49 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,8 +6,10 @@
 #Stop the docker-compose
 sudo docker-compose stop
 #Re-build the images
+clear
 sudo docker build -t openmetasiata/api:1.0.0beta api/ 
 sudo docker build -t openmetasiata/db:1.0.0beta db/ 
 sudo docker build -t openmetasiata/web:1.0.0beta webPage/
 #Execute again the docker-compose
+clear
 sudo docker-compose -f docker-compose.yml up -d
