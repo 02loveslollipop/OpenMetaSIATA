ffbe6652de0ac3880b5df6e486a6107ee0dcea47
02loveslollipop <mmonsalve2005@gmail.com>
2023-05-07 22:47:27 -0500

    Updated config.yml

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ sudo docker build -t openmetasiata/api:1.0.0beta api/
 sudo docker build -t openmetasiata/db:1.0.0beta db/ 
 sudo docker build -t openmetasiata/web:1.0.0beta webPage/
 
-sudo docker run -it -p 5000:5000  openmetasiata/api:1.0.0beta
-sudo docker run -it -p 6969:6969 openmetasiata/db:1.0.0beta
-sudo docker run -it -p 80:80 openmetasiata/web:1.0.0beta
+sudo docker run -d -p 5000:5000  openmetasiata/api:1.0.0beta
+sudo docker run -d -p 6969:6969 openmetasiata/db:1.0.0beta
+sudo docker run -d -p 80:80 openmetasiata/web:1.0.0beta
