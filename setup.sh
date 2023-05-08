3e55f26177527ce7e400274e520d3cefd5e43e83
02loveslollipop <mmonsalve2005@gmail.com>
2023-05-07 22:29:18 -0500

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@ sudo apt update
 sudo apt install docker-compose
 sudo docker build -t openmetasiata/api:1.0.0beta api/ 
 sudo docker build -t openmetasiata/db:1.0.0beta db/ 
-sudo docker build -t openmetasiata/web:1.0.0beta webPage/ 
+sudo docker build -t openmetasiata/web:1.0.0beta webPage/
+
+sudo docker run -it -p 5000:5000  openmetasiata/api:1.0.0beta
+sudo docker run -it -p 6969:6969 openmetasiata/db:1.0.0beta
+sudo docker run -it -p 80:80 openmetasiata/web:1.0.0beta
