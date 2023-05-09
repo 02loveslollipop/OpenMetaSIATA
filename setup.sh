b2217e9f5399f271112f5815d04151cd2e19fd3d
02loveslollipop <mmonsalve2005@gmail.com>
2023-05-08 21:54:13 -0500

    Updated setup.sh to use docker-compose

--- setup.sh
+++ setup.sh
@@ -7,6 +7,4 @@ sudo docker build -t openmetasiata/api:1.0.0beta api/
 sudo docker build -t openmetasiata/db:1.0.0beta db/ 
 sudo docker build -t openmetasiata/web:1.0.0beta webPage/
 #Runing docker-compose
-sudo docker run -d -p 5000:5000  openmetasiata/api:1.0.0beta
-sudo docker run -d -p 6969:6969 openmetasiata/db:1.0.0beta
-sudo docker run -d -p 80:80 openmetasiata/web:1.0.0beta
+sudo docker-compose -f docker-compose.yml up -d
